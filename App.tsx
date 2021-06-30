d864d632d6893fdd7c138c9ca94483ef0e98b5b6
KIMBEOBWOO <qjqdn1568@naver.com>
2021-06-30 14:29:40 +0900

    dummy page 생성

--- App.tsx
+++ App.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
-import {Text} from 'react-native-elements';
+
+import TestScreen from './src/components/screens/test/testScreen';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.testContainer}>
-      <Text h1 style={styles.testText}>
-        Owner Mobile Scanner{'\n'}App.tsx
-      </Text>
+      <TestScreen />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   testContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#fdbd39',
+    // flex: 1,
+    // justifyContent: 'center',
+    // backgroundColor: '#fdbd39',
   },
   testText: {
     fontSize: 40,
---
+++ src/components/screens/test/testScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {Text} from 'react-native-elements';
+
+function TestScreen() {
+  return (
+    <SafeAreaView
+      style={{
+        backgroundColor: '#fdbd39',
+      }}>
+      <Text>aaa</Text>
+    </SafeAreaView>
+  );
+}
+
+export default TestScreen;
