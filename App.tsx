92297ad48e44a180baa0cba1420e26317fb4019d
KIMBEOBWOO <qjqdn1568@naver.com>
2021-07-07 18:09:18 +0900

    점주주 바코드 스캔 완료 후 페이지 1. ios 스택 이동 animation 으로 통일

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
 import { StackParamList } from './src/navigations/stack-param-list/StackParamList';
 import Test from './src/components/screens/home/Test';
 import BarcodeRecognitionComplete from './src/components/screens/barcode-recognition-complete/BarcodeRecognitionComplete';
 import { configure } from 'axios-hooks';
 import axios from './src/utils/axios';
 
+/* IOS stack 이동 animation options */
+const TransitionScreenOptions = {
+	...TransitionPresets.SlideFromRightIOS,
+};
 const Stack = createStackNavigator<StackParamList>();
 
 configure({ axios: axios.axiosInstance });
@@ -14,7 +18,7 @@ configure({ axios: axios.axiosInstance });
 const App = () => {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator>
+			<Stack.Navigator screenOptions={TransitionScreenOptions}>
 				<Stack.Screen name="테스트" component={Test} />
 				<Stack.Screen
 					name="바코드 인식 완료"
