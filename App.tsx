c17f30aeccd86eacac627e5e570089bcb146e30a
harrysci <59914405+harrysci@users.noreply.github.com>
2021-07-08 18:38:47 +0900

    stack 추가 완료

--- App.tsx
+++ App.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import BottomNav from './src/navigations/BottomNav';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
+import { StackParamList } from './src/navigations/stack-param-list/StackParamList';
+const Stack = createStackNavigator<StackParamList>();
 
 const App = () => {
-  return <BottomNav />;
+	return (
+		<NavigationContainer>
+			<Stack.Navigator >
+				<Stack.Screen
+					name="메인화면"
+					component={BottomNav}
+					options={{ headerShown: false }}
+				/>
+				{/* <Stack.Screen
+					name="상품 상세 정보"
+					component={ProductDetail}
+					options={{ headerTitle: '' }}
+				/> */}
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 };
 
 export default App;
