649538afe9d913046c83db31642d1052462cdffb
harrysci <59914405+harrysci@users.noreply.github.com>
2021-07-09 12:08:24 +0900

    필요없는 주석 제거

--- src/components/screens/home/product-list/ListScreen.tsx
+++ src/components/screens/home/product-list/ListScreen.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import {ListItem} from 'react-native-elements';
-import {Image} from 'react-native-elements/dist/image/Image';
+import {SafeAreaView, View} from 'react-native';
 import ProductListComponent from '../../../organisms/product-list/ProductListComponent';
 import UpContainer from '../../../organisms/product-list/UpContainer';
 import styles from './style';
