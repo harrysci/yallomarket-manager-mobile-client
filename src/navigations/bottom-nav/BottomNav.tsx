3ee837fde0c3b2d9c101041e545fbc164309a247
BeomChang <fnzlvkdvkd@naver.com>
2021-07-15 13:46:09 +0900

    bottom navigator text style modify

--- src/navigations/bottom-nav/BottomNav.tsx
+++ src/navigations/bottom-nav/BottomNav.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import BottomNavStyles from './styles/BottomNavStyles';
 
 import ListScreen from '../../components/screens/home/product-list/ListScreen';
-import { ScannerScreen } from '../../components/screens/barcode/ScannerScreen';
 import BarcodeRecognitionComplete from '../../components/screens/barcode-recognition-complete/BarcodeRecognitionComplete';
-import { RouteProp, useRoute } from '@react-navigation/native';
-import { StackParamList } from '../stack-param-list/StackParamList';
 
 const Tab = createBottomTabNavigator();
 
 const BottomNav = (): JSX.Element => {
-	
 	return (
-		// <NavigationContainer>
 		<Tab.Navigator
-			
 			screenOptions={({ route }) => ({
 				tabBarIcon: ({ focused }) => {
 					if (route.name === '바코드 스캔') {
@@ -58,13 +52,16 @@ const BottomNav = (): JSX.Element => {
 				activeTintColor: '#000000',
 				inactiveTintColor: '#B7B7B7',
 				style: BottomNavStyles.tabBar,
+				labelStyle:
+					Platform.OS === 'android'
+						? BottomNavStyles.androidLabelStyle
+						: BottomNavStyles.iosLabelStyle,
 			}}
 		>
 			{/* 테스트를 위해 주석처리  <Tab.Screen name="바코드 스캔" component={ScannerScreen} /> */}
 			<Tab.Screen name="바코드 스캔" component={BarcodeRecognitionComplete} />
 			<Tab.Screen name="등록 목록" component={ListScreen} />
 		</Tab.Navigator>
-		// </NavigationContainer>
 	);
 };
 
