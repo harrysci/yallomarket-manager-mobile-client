1527adf1ec59a8bcb7c4b0568ec060616d31429f
harrysci <59914405+harrysci@users.noreply.github.com>
2021-08-05 12:05:47 +0900

    상품 게시일별

--- src/components/organisms/product-list/ProductListComponent.tsx
+++ src/components/organisms/product-list/ProductListComponent.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { ActivityIndicator, FlatList, SafeAreaView } from 'react-native';
+import { ActivityIndicator, FlatList, SafeAreaView, SectionList, Text } from 'react-native';
 import OneProductComponent from './OneProductComponent';
 import useAxios from 'axios-hooks';
 import { GetImageProductListRes } from '../../screens/home/dto/GetImageProductListDto';
 import { ListItem } from 'react-native-elements';
-
+import moment from 'moment';
 export interface ProductListComponentProps {
 	overState: boolean;
 	handleUpdateCompleteOverlay: () => void;
 	handleDeleteOverlay: () => void;
 }
-
+export interface SectionListProps {
+	title: Date;
+	getData: GetImageProductListRes[] | undefined;
+}
 const ProductListComponent = (props: ProductListComponentProps) => {
 	const [{ data: getData, loading: getLoading, error: getError }, executeGet] = useAxios<
 		GetImageProductListRes[]
@@ -22,7 +25,21 @@ const ProductListComponent = (props: ProductListComponentProps) => {
 	const executeGetHandler = () => {
 		executeGet();
 	};
-
+	const sectionList = getData?.map(each => moment(each.productCreatedAt).format('YYYY/ MM/ DD'));
+	const set = new Set(sectionList);
+	const newSectionList = [...set];
+	const sectionData = newSectionList?.map(each => ({
+		title: each,
+		data: getData?.filter(i => moment(i.productCreatedAt).format('YYYY/ MM/ DD') === each),
+	}));
+	// const renderItem = ({ item }: { item: GetImageProductListRes }) => (
+	// 	<OneProductComponent
+	// 		productData={item}
+	// 		executeGetHandler={executeGetHandler}
+	// 		handleUpdateCompleteOverlay={props.handleUpdateCompleteOverlay}
+	// 		handleDeleteOverlay={props.handleDeleteOverlay}
+	// 	/>
+	// );
 	const renderItem = ({ item }: { item: GetImageProductListRes }) => (
 		<OneProductComponent
 			productData={item}
@@ -31,14 +48,18 @@ const ProductListComponent = (props: ProductListComponentProps) => {
 			handleDeleteOverlay={props.handleDeleteOverlay}
 		/>
 	);
-
 	return (
 		<SafeAreaView style={{ backgroundColor: 'white', flex: 1 }}>
 			{!getLoading && !getError && getData && (
-				<FlatList
-					data={getData}
+				<SectionList
+					sections={sectionData}
 					renderItem={renderItem}
-					keyExtractor={item => String(item.productId)}
+					//keyExtractor={item => String(item.productId)}
+					renderSectionHeader={({ section: { title } }) => (
+						<Text style={{ marginHorizontal: 17, fontSize: 9 }}>
+							상품 게시일 {title}
+						</Text>
+					)}
 				/>
 			)}
 		</SafeAreaView>
